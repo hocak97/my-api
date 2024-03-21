70b1b7234d149d5c759695025e71e7055f4aa0b5
hocak97 <hovanhoi16061997@gmail.com>
2024-03-21 15:13:50 +0700

    Update code

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
-import express from 'express';
-// const express = require('express');
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const app = express();
-const hostname = 'localhost';
-const port = 3000;
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening on port http://${hostname}:${port}/`)
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
 })
