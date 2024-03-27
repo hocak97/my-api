09f082218dd4ef3a3a5b5c34473c46e85cbc10c7
hocak97 <hovanhoi16061997@gmail.com>
2024-03-27 23:41:53 +0700

    Update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
-  'http://localhost:8017',
+  // 'http://localhost:5173',
+  // 'http://localhost:8017',
   'my-project-henna-chi.vercel.app'
 
 ];
